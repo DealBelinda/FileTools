f78564717b034ec715bd59f344fc2c72a4f093bd
suhongwei <670892861@qq.com>
2016-10-11 12:46:59 +0800

    删除不相关文件

--- BJticketsystem/Application/Home/Conf/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-	'AUTOLOAD_NAMESPACE' => array(
-    'Lib'     => APP_PATH.'/Home/Lib',
-    )
-);
--- BJticketsystem/vendor/pingplusplus/pingpp-php/lib/text.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Pingpp;
-
-class text{
-    static function sayHello(){
-    	echo 'helloworld';
-    }
-
-}
